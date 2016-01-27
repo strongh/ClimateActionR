defdd5229564ac3d228aba8542aa106239166b81
wpetry <wpetry@uci.edu>
2016-01-26 16:21:00 -0800

    initial commit

---
+++ CMIP5_streamflow.R
@@ -0,0 +1,39 @@
+#################################################-
+## DOMAIN: HYDROLOGY
+## Retrieve downsampled CMIP-5 streamflow projections at NCAR stream monitoring sites
+## URL: http://gdo-dcp.ucllnl.org/downscaled_cmip_projections/
+## Author: W. Petry
+#################################################-
+## Preliminaries
+library(dplyr)
+library(magrittr)
+
+#################################################-
+## Construct URLs to retrieve Colorado River basin data
+#################################################-
+# Retrieve site list
+site.list<-read.table("ftp://gdo-dcp.ucllnl.org/pub/dcp/archive/cmip5/hydro/routed_streamflow/NCAR_cmip5_streamflow_sites.txt",sep="\t")
+names(site.list)<-c("site.abb","lat","long","basin","id","site.name")
+
+# Build FTP URLs
+site.list %<>% mutate(FTP=paste0("ftp://gdo-dcp.ucllnl.org/pub/dcp/archive/cmip5/hydro/routed_streamflow/cmip5_ncar_mon/streamflow_cmip5_ncar_month_",site.abb,".csv.zip"),
+                      file.name=paste0("streamflow_cmip5_ncar_month_",site.abb,".csv"))
+
+# Filter to desired sites (a) in Colorado River Basin and (b) along the Colorado River proper
+crb.sites<-site.list %>% filter(basin=="COLO") # _C_olorado _R_iver _B_asin sites
+cr.sites<-crb.sites %>% filter(grepl("^Colorado R",site.name)) # _C_olorado _R_iver sites (sensu stricto)
+
+#################################################-
+## Download data to shared Dropbox folder
+#################################################-
+setwd("~/Dropbox/ClimateActionRData/")
+
+for(i in 1:nrow(cr.sites)){
+  tf<-tempfile()
+  download.file(cr.sites$FTP[i],tf,mode="wb")
+  unzip(tf,files=paste0("cmip5_ncar_mon/",cr.sites$file.name[i]),junkpaths=T)
+  unlink(tf)
+}
+
+
+
