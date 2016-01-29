ce651304a852fbb3fc7cfd477cf7de1d2f4839a3
wpetry <wpetry@uci.edu>
2016-01-28 23:25:01 -0800

    add creation of single datafile

--- CMIP5_streamflow.R
+++ CMIP5_streamflow.R
@@ -5,8 +5,11 @@
 ## Author: W. Petry
 #################################################-
 ## Preliminaries
+library(plyr)
 library(dplyr)
 library(magrittr)
+library(rdrop2)
+library(tidyr)
 
 #################################################-
 ## Construct URLs to retrieve Colorado River basin data
@@ -26,14 +29,30 @@ cr.sites<-crb.sites %>% filter(grepl("^Colorado R",site.name)) # _C_olorado _R_i
 #################################################-
 ## Download data to shared Dropbox folder
 #################################################-
-setwd("~/Dropbox/ClimateActionRData/")
+# Writes separate csv for each monitoring station to working directory
+# for(i in 1:nrow(cr.sites)){
+#   tf<-tempfile()
+#   download.file(cr.sites$FTP[i],tf,mode="wb")
+#   unzip(tf,files=paste0("cmip5_ncar_mon/",cr.sites$file.name[i]),junkpaths=T)
+#   unlink(tf)
+# }
 
+# Combines into single csv containing all monitoring stations, write to Dropbox
+dat.list<-vector(mode="list")
 for(i in 1:nrow(cr.sites)){
   tf<-tempfile()
   download.file(cr.sites$FTP[i],tf,mode="wb")
-  unzip(tf,files=paste0("cmip5_ncar_mon/",cr.sites$file.name[i]),junkpaths=T)
+  dat.list[[i]]<-read.csv(unz(tf,filename=paste0("cmip5_ncar_mon/",cr.sites$file.name[i])))
+  dat.list[[i]]$Station<-as.character(cr.sites$site.abb[i])
   unlink(tf)
 }
 
+# Prepare data for output to a single csv file
+output.df<-plyr::ldply(dat.list) %>%
+  gather(GCM.string,streamflow,access1.0_rcp45_r1i1p1:noresm1.m_rcp85_r1i1p1) %>%
+  separate(GCM.string,c("GCM","Scenario","excess"),sep="_",remove=F) %>%
+  select(-excess)
 
+write.csv(output.df,"CMIP5_streamflow.csv")
+drop_upload("CMIP5_streamflow.csv",dest="ClimateActionRData")
 
