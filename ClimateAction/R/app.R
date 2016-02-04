a3eccc42d10b361ee84466201918cd8759ffa6d4
Homer Strong <homer.strong@gmail.com>
2016-02-03 17:51:27 -0800

    checkin first pass at shiny viewer

---
+++ ClimateAction/R/app.R
@@ -0,0 +1,56 @@
+#' View Climate
+#'
+#' Starts a Shiny app for viewing climate data.
+#' @param start
+#' @param end
+#' @keywords download
+#' @export
+#' @examples
+#' shiny_viewer()
+shiny_viewer <- function() {
+  require(shiny)
+  require(ggplot2)
+  require(dplyr)
+  require(rdrop2)
+  data.files <- drop_dir("/ClimateActionRData") %>% filter(mime_type=="text/csv")
+  shinyApp(
+    ui = fluidPage(
+      titlePanel("Climate Action Data Viewer"),
+      
+      sidebarLayout(
+        sidebarPanel(
+          helpText("Examine spatial climate data."),
+          selectInput("dataset", "Data:", data.files$path),
+          "Code:",
+          verbatimTextOutput("example.code")
+        ),
+        
+        mainPanel(
+          tabsetPanel(type = "tabs",
+                      tabPanel("Time Series",
+                               plotOutput("time.series.plot")
+                            ),
+                      tabPanel("Table",
+                               dataTableOutput("table"))
+                      
+          )   )
+      )
+    ),
+    server = function(input, output) {
+      output$table <- renderDataTable({
+        df <- drop_read_csv(input$dataset)
+        df
+      })
+      output$time.series.plot <- renderPlot({
+        df <- drop_read_csv(input$dataset) %>% filter(state=="California")
+        if("year" %in% names(df))
+          ggplot(df, aes_string("year", names(df)[-ncol(df)])) + geom_line()
+      })
+      output$example.code <- renderText({
+        paste0("R> ", "drop_read_csv(", input$dataset, ")")
+      })
+    }
+  )
+}
+
+shiny_viewer()
