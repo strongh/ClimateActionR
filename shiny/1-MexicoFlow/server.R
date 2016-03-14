be7eb3950e0430f9fbea11d86d9c872111157b2d
Homer Strong <homer.strong@gmail.com>
2016-03-13 17:06:41 -0700

    updates to shiny v 1

--- shiny/1-MexicoFlow/server.R
+++ shiny/1-MexicoFlow/server.R
@@ -1,32 +1,13 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define server logic required to draw a histogram
-#flow_data <- read.csv("~/code/ClimateActionR/CMIP5_streamflow.csv") 
-
-## coords of southernmost station. not sure this is 
-## the right one
-south_station_coords <- c("-114.470", "32.880")
-
-## need to figure out how to coordinate these summary calcs
- station_flows <- flow_data %>%
-  filter(lat < 33, Scenario=="rcp26", GCM=="bcc.csm1.1")
-## station_flows <- read.csv("~/code/ClimateActionR/flow_data.csv")
-## station_yearly_flows <- ddply(station_flows, .(Year), summarise, streamflow=sum(streamflow))
-station_yearly_flows <- read.csv("~/code/ClimateActionR/yearly_flow.csv")
-station.coords <- read.csv("~/code/ClimateActionR/station_coords.csv") # unique(flow_data[, c("long", "lat")])
+station_yearly_flows <- read.csv("~/catdata/station_yearly_flows.csv")
+station.coords <- read.csv("~/catdata/station_coords.csv") # unique(flow_data[, c("long", "lat")])
 theme_set(theme_minimal())
-#states <- geom_shape("admin_boundaries", "state_boundaries")
+state_shapes <- read.csv("~/catdata/state_shapes.csv")
+states <- geom_path(data=state_shapes,
+                    aes(group=group))
+
 shinyServer(function(input, output) {
-  
   output$stationMap <- renderPlot({
     ggplot(station.coords, aes(long, lat)) + 
       geom_point(size=3, colour="red") + states
--- shiny/1-MexicoFlow/ui.R
+++ shiny/1-MexicoFlow/ui.R
@@ -1,17 +1,9 @@
-## get data, just needs to be run once
-
-#library(rdrop2)
-#write.csv(rdrop2::drop_read_csv("/ClimateActionRData/CMIP5_streamflow.csv"),
-#          file="CMIP5_streamflow.csv")
-
 library(shiny)
-## Assumes that 
 
 shinyUI(fluidPage(
   # Application title
   titlePanel("Mexico Stream Flow"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
        "This first app shows a simple map of
@@ -20,7 +12,6 @@ shinyUI(fluidPage(
        station."
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(
        tabPanel("Flow",
